0b0f17502a6f556bd9fe6e258033c4d65fffdb41
rimever <mot_siva_ordin@hotmail.com>
2019-09-01 20:33:07 +0900

    簡単なAPいを実装

---
+++ AspNetCoreMvcEf/AspNetCoreMvcEf/Models/PeopleDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreMvcEf.Models
+{
+    public class PeopleDbContext:DbContext
+    {
+        public PeopleDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlite("Data Source=people.db");
+    }
+
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ AspNetCoreMvcEf/AspNetCoreMvcEf/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreMvcEf.Models;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetCoreMvcEf
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<PeopleDbContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+
+    public static class DbInitializer
+    {
+        public static void Initialize(PeopleDbContext context)
+        {
+            
+            context.Database.EnsureCreated();
+
+            // Look for any students.
+            if (context.Persons.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Add(new Person() { Name = "John"});
+            context.Add(new Person() { Name = "Thomas"});
+            context.SaveChanges();
+        }
+    }
+}
